b3a93842660c5fbe017ccbff9525351800799eab
Dwaraka Nath <dtsdwarak@hotmail.com>
2022-12-07 23:48:48 +0530

    [Big Bang Change] CSS/Layout Refresh ✨

--- Gemfile
+++ Gemfile
@@ -2,6 +2,3 @@ source "https://rubygems.org"
 ruby RUBY_VERSION
 
 gem 'jekyll', '~> 3.8.7'
-
-# For pagination
-gem 'jekyll-paginate', '~> 1.1'
--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ import child from 'child_process'
 import gulp from 'gulp'
 import gutil from 'gulp-util'
 import concat from 'gulp-concat'
-import minify from 'gulp-babel-minify'
 import cleanCSS from 'gulp-clean-css'
 import imagemin from 'gulp-imagemin'
 import imageminGifsicle from 'imagemin-gifsicle'
@@ -18,7 +17,6 @@ const browserSync = browsersync.create();
 const siteRoot = "_site";
 
 const cssDevFiles = "css/*.?(s)css";
-const jsDevFiles = ["js/jquery.min.js", "js/bootstrap.min.js"];
 
 gulp.task("compressImages", () =>
   gulp
@@ -52,13 +50,6 @@ gulp.task("compressImages", () =>
     .pipe(gulp.dest("assets/images"))
 );
 
-gulp.task("concatJS", () => {
-  return gulp
-    .src(jsDevFiles)
-    .pipe(concat("site.js"))
-    .pipe(gulp.dest("assets/js"));
-});
-
 gulp.task("concatCSS", () => {
   return gulp
     .src([cssDevFiles])
@@ -66,19 +57,6 @@ gulp.task("concatCSS", () => {
     .pipe(concat("site.css"))
     .pipe(gulp.dest("assets/css"));
 });
-
-gulp.task(
-  "minifyJS",
-  gulp.series("concatJS", () => {
-    return gulp
-      .src("assets/js/site.js")
-      .pipe(minify())
-      .on("error", (err) => {
-        gutil.log(gutil.colors.red("[Error]"), err.toString());
-      })
-      .pipe(gulp.dest("assets/js"));
-  })
-);
 
 gulp.task(
   "minifyCSS",
@@ -155,8 +133,8 @@ gulp.task("serve", function (done) {
   done();
 });
 
-gulp.task("release", gulp.series(["minifyCSS", "minifyJS", "build"]));
+gulp.task("release", gulp.series(["minifyCSS", "build"]));
 gulp.task(
   "default",
-  gulp.series(["minifyCSS", "minifyJS", "build-watch", "serve"])
+  gulp.series(["minifyCSS", "build-watch", "serve"])
 );
